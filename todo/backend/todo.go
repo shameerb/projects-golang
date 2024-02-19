09f41ee7f7ac3a4d237490ab425b6be0390aa269
Shameer <shameer789@gmail.com>
2024-02-20 00:09:48 +0530

    updated submodule

--- todo/backend/todo.go
+++ todo/backend/todo.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
-	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
+	"strconv"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"encoding/json"
-	"strconv"
 	"github.com/rs/cors"
+	log "github.com/sirupsen/logrus"
 )
 
 var db, _ = gorm.Open("mysql", "user:password@/todolist?charset=utf8&parseTime=True&loc=Local")
 
-type TodoItemModel struct{
-	Id int `gorm:"primary_key"`
+type TodoItemModel struct {
+	Id          int `gorm:"primary_key"`
 	Description string
-	Completed bool
+	Completed   bool
 }
 
 func CreateItem(w http.ResponseWriter, r *http.Request) {
@@ -47,7 +48,7 @@ func UpdateItem(w http.ResponseWriter, r *http.Request) {
 		todo.Completed = completed
 		db.Save(&todo)
 		w.Header().Set("Content-Type", "application/json")
-		io.WriteString(w,`{"updated": true}`)
+		io.WriteString(w, `{"updated": true}`)
 	}
 }
 
@@ -88,7 +89,6 @@ func GetTodoItems(completed bool) interface{} {
 	return TodoItems
 }
 
-
 func GetItemById(Id int) bool {
 	todo := &TodoItemModel{}
 	result := db.First(&todo, Id)
@@ -104,7 +104,6 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
 }
-
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{})
